36bd1f2f19424e22357725b5c2581554f3440c9f
Risyanggi Azmi Faizin <risyanggi@gmail.com>
2023-05-13 17:28:00 +0700

    use c-c to noh

--- vim/lua/rafgugi/remap.lua
+++ vim/lua/rafgugi/remap.lua
@@ -1,6 +1,5 @@
 -- move horizontal clears search highlighting
-vim.keymap.set("n", "h", "h:noh<CR>", { silent = true })
-vim.keymap.set("n", "l", "l:noh<CR>", { silent = true })
+vim.keymap.set("n", "<C-c>", "<C-c>:noh<CR>", { silent = true })
 
 -- Temporary map from learning.
 vim.keymap.set("i", "<Esc>", "<Esc>:echo 'Press jk to escape'<CR>gi")
